bfeb7df872d018af483eb48380fb475407a0d99f
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-12-07 14:15:43 +0100

    move to c:\run

--- artifacts/Invoke-DownloadAndImportArtifact.ps1
+++ artifacts/Invoke-DownloadAndImportArtifact.ps1
@@ -3,17 +3,17 @@ param (
     [string]$url, 
     [string]$pat = "",
     [string]$exclude = "*Tests_*.app",
-    [string]$ppiArtifactUtils = "c:\run\my\PPIArtifactUtils.psd1",
-    [string]$ppiArtifactSettings = "c:\run\my\ArtifactSettings.ps1"
+    [string]$ppiArtifactUtils = "c:\run\PPIArtifactUtils.psd1",
+    [string]$ppiArtifactSettings = "c:\run\ArtifactSettings.ps1"
 )
 
 try {
     if (Test-Path $ppiArtifactUtils) { Import-Module $ppiArtifactUtils -DisableNameChecking -Force -ErrorAction SilentlyContinue }
-    . "c:\run\my\ArtifactSettings.ps1"
+    . "c:\run\ArtifactSettings.ps1"
 
     $started = Get-Date -Format "o"
 
-    $targetDir  = "$($env:TEMP)/$([System.IO.Path]::GetRandomFileName())"
+    $targetDir = "$($env:TEMP)/$([System.IO.Path]::GetRandomFileName())"
     if (! $Tenantid) { $Tenantid = "default" }
     
     Write-Host "$([System.Environment]::NewLine)Download Artifact ..."
@@ -22,10 +22,11 @@ try {
     Import-Artifacts -Path $targetDir -NavServiceName $NavServiceName -ServerInstance $ServerInstance -Tenant $Tenantid -OperationScope "Download and Import Artifact - " -telemetryClient $telemetryClient
     Write-Host "Done"
     
-    $properties = @{"organization" = $organization; "project" = $project; "feed" = $feed; "name" = $name; "scope" = $scope; "view" = $view; "protocolType" = $type; "url" = $url_output}
+    $properties = @{"organization" = $organization; "project" = $project; "feed" = $feed; "name" = $name; "scope" = $scope; "view" = $view; "protocolType" = $type; "url" = $url_output }
     Invoke-LogOperation -name "Download and Import Artifact" -success $success -started $started -properties $properties -telemetryClient $telemetryClient
     
     return $result
-} catch {
+}
+catch {
     Invoke-LogError -exception $_.Exception -telemetryClient $telemetryClient -operation "Download and Import Artifact"
 }
