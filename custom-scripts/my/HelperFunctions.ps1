8ea532ac1754933b7101f1cba557f81534fc4581
Markus Lippert <lippertmarkus@gmx.de>
2021-02-10 12:44:40 +0100

    feat: support custom scripts

---
+++ custom-scripts/my/HelperFunctions.ps1
@@ -0,0 +1,34 @@
+# invoke default
+. (Join-Path $runPath $MyInvocation.MyCommand.Name)
+
+if ([string]::IsNullOrEmpty($env:useCustomScriptsFromRepo) -or $($env:useCustomScriptsFromRepo).ToLower() -ne "true") {
+    return;
+}
+
+Write-Host "Downloading custom run scripts from directory .container-my of repo ..."
+
+if ([string]::IsNullOrEmpty($env:CcOrgName) -or [string]::IsNullOrEmpty($env:CcProjectId) -or [string]::IsNullOrEmpty($env:CcRepoId) -or [string]::IsNullOrEmpty($env:AZURE_DEVOPS_EXT_PAT)) {
+    Write-Warning "CcOrgName, CcProjectId, CcRepoId or AZURE_DEVOPS_EXT_PAT is empty, can't download custom scripts"
+    return;
+}
+
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("dummy:$($env:AZURE_DEVOPS_EXT_PAT)"))
+$Headers = @{
+    Authorization = "Basic $encodedCreds"
+}
+
+$url = "https://dev.azure.com/$($env:CcOrgName)/$($env:CcProjectId)/_apis/git/repositories/$($env:CcRepoId)/items?path=%2F.container-my&download=true&resolveLfs=true&%24format=zip&api-version=5.0"
+$ProgressPreference = "SilentlyContinue"
+
+try {
+    $downloadTarget = "C:\myscripts.zip"
+    $extractionTarget = "C:\myscripts"
+    $destination = "C:\Run\my"
+    Invoke-WebRequest -Uri $url -UseBasicParsing -Headers $Headers -OutFile $downloadTarget
+    Expand-Archive $downloadTarget -DestinationPath $extractionTarget
+    Get-ChildItem -Path "$extractionTarget\.container-my\*" -Recurse | Move-Item -Destination $destination -Force
+}
+catch {
+    Write-Warning "Couldn't download custom scripts from $url, error: $($_)"
+    return;
+}
