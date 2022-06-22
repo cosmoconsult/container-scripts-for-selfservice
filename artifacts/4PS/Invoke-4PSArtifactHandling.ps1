1c1f96fdc7b30da2488a73f710e33764bdc59b7b
Tobias Fenster <tfenster@4psbau.de>
2022-06-22 22:44:06 +0200

    check if demodagta available

--- artifacts/4PS/Invoke-4PSArtifactHandling.ps1
+++ artifacts/4PS/Invoke-4PSArtifactHandling.ps1
@@ -45,116 +45,118 @@ function Invoke-4PSArtifactHandling {
                 $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securepassword)
                 $unsecurepassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
-                $files = Get-ChildItem "c:\demodata" -Filter *.xml | Sort-Object Name -Descending
-                $firstRun = $true
-                foreach ($demoDataFile in $files) {
-                    $demoDataFileName = $demoDataFile | ForEach-Object { $_.Name }
-                    "  Using XML file {0}" -f $demoDataFile.FullName | Write-Host 
-                    if ($demoDataFileName -match 'DemoData_(.*)_.xml') {
-                        $companyName = $Matches[1]
-                        Write-Host "  Create and initialize company $companyName"
-                        New-NAVCompany -CompanyName $companyName -ServerInstance BC
+                if (Test-Path -Path "c:\demodata") {
+                    $files = Get-ChildItem "c:\demodata" -Filter *.xml | Sort-Object Name -Descending
+                    $firstRun = $true
+                    foreach ($demoDataFile in $files) {
+                        $demoDataFileName = $demoDataFile | ForEach-Object { $_.Name }
+                        "  Using XML file {0}" -f $demoDataFile.FullName | Write-Host 
+                        if ($demoDataFileName -match 'DemoData_(.*)_.xml') {
+                            $companyName = $Matches[1]
+                            Write-Host "  Create and initialize company $companyName"
+                            New-NAVCompany -CompanyName $companyName -ServerInstance BC
 
-                        Write-Host "    Init setup tables"
-                        Invoke-NavCodeunit `
-                            -ServerInstance BC `
-                            -CompanyName $companyName `
-                            -Codeunitid 2 `
-                            -MethodName 'InitSetupTables' `
-                            -TimeZone ServicesDefaultTimeZone `
-                            -ErrorAction SilentlyContinue 
-                        
-                        if ($env:IsBuildContainer -ne "true") {
-                            Write-Host "    Import setup data from XML file"
+                            Write-Host "    Init setup tables"
                             Invoke-NavCodeunit `
                                 -ServerInstance BC `
                                 -CompanyName $companyName `
-                                -CodeunitId 11012268 `
-                                -MethodName ImportSetupDataFromXmlFile `
-                                -Argument "$($demoDataFile.FullName)"
-                        } else {
-                            Write-Host "    Skip import setup data from XML file as this seems to be a build container"
-                        }                
+                                -Codeunitid 2 `
+                                -MethodName 'InitSetupTables' `
+                                -TimeZone ServicesDefaultTimeZone `
+                                -ErrorAction SilentlyContinue 
                             
-                        Write-Host "    Run manual data upgrade 4PS"
-                        Invoke-NavCodeunit `
-                            -ServerInstance BC `
-                            -CompanyName $companyName `
-                            -CodeunitId 50189 `
-                            -MethodName RunManualDataUpgrade `
-                            -Argument "$firstRun"
-                            
-                        if ($env:IsBuildContainer -ne "true") {
-                            Write-Host "    Initialize FSA setup"
+                            if ($env:IsBuildContainer -ne "true") {
+                                Write-Host "    Import setup data from XML file"
+                                Invoke-NavCodeunit `
+                                    -ServerInstance BC `
+                                    -CompanyName $companyName `
+                                    -CodeunitId 11012268 `
+                                    -MethodName ImportSetupDataFromXmlFile `
+                                    -Argument "$($demoDataFile.FullName)"
+                            } else {
+                                Write-Host "    Skip import setup data from XML file as this seems to be a build container"
+                            }                
+                                
+                            Write-Host "    Run manual data upgrade 4PS"
                             Invoke-NavCodeunit `
                                 -ServerInstance BC `
                                 -CompanyName $companyName `
                                 -CodeunitId 50189 `
-                                -MethodName InitializeFSASetup
-
-                            Write-Host "    Initialize OSA setup"
-                            Invoke-NavCodeunit `
-                                -ServerInstance BC `
-                                -CompanyName $companyName `
-                                -CodeunitId 50189 `
-                                -MethodName InitializeOSASetup
-
-                            if ($firstRun) {
-                                Write-Host "    Initialize WebServices"
+                                -MethodName RunManualDataUpgrade `
+                                -Argument "$firstRun"
+                                
+                            if ($env:IsBuildContainer -ne "true") {
+                                Write-Host "    Initialize FSA setup"
                                 Invoke-NavCodeunit `
                                     -ServerInstance BC `
                                     -CompanyName $companyName `
                                     -CodeunitId 50189 `
-                                    -MethodName PublishAllWebServices
+                                    -MethodName InitializeFSASetup
 
-                                Write-Host "    Initialize FSA"
+                                Write-Host "    Initialize OSA setup"
                                 Invoke-NavCodeunit `
                                     -ServerInstance BC `
                                     -CompanyName $companyName `
                                     -CodeunitId 50189 `
-                                    -MethodName InitializeFSA
+                                    -MethodName InitializeOSASetup
 
-                                Write-Host "    Initialize OSA"
-                                Invoke-NavCodeunit `
-                                    -ServerInstance BC `
-                                    -CompanyName $companyName `
-                                    -CodeunitId 11128546 `
-                                    -MethodName InitializeOSA
+                                if ($firstRun) {
+                                    Write-Host "    Initialize WebServices"
+                                    Invoke-NavCodeunit `
+                                        -ServerInstance BC `
+                                        -CompanyName $companyName `
+                                        -CodeunitId 50189 `
+                                        -MethodName PublishAllWebServices
 
-                                Write-Host "    Initialize License"
-                                Invoke-NavCodeunit `
+                                    Write-Host "    Initialize FSA"
+                                    Invoke-NavCodeunit `
+                                        -ServerInstance BC `
+                                        -CompanyName $companyName `
+                                        -CodeunitId 50189 `
+                                        -MethodName InitializeFSA
+
+                                    Write-Host "    Initialize OSA"
+                                    Invoke-NavCodeunit `
+                                        -ServerInstance BC `
+                                        -CompanyName $companyName `
+                                        -CodeunitId 11128546 `
+                                        -MethodName InitializeOSA
+
+                                    Write-Host "    Initialize License"
+                                    Invoke-NavCodeunit `
+                                        -ServerInstance BC `
+                                        -CompanyName $companyName `
+                                        -CodeunitId 50189 `
+                                        -MethodName CreateLicenses
+                                    $firstRun = $false
+                                }
+
+                                Write-Host "    Initialize General User ($username / $unsecurepassword) in $companyName"
+                                Invoke-NAVCodeunit `
                                     -ServerInstance BC `
                                     -CompanyName $companyName `
                                     -CodeunitId 50189 `
-                                    -MethodName CreateLicenses
-                                $firstRun = $false
+                                    -MethodName CreateGeneralAppUser `
+                                    -Argument "$($username.PadRight(100))$($unsecurepassword.PadRight(64))"
+
+                                Write-Host "    Initialize FSA User"
+                                Invoke-NAVCodeunit `
+                                    -ServerInstance BC `
+                                    -CompanyName $companyName `
+                                    -CodeunitId 50189 `
+                                    -MethodName CreateFSAUser `
+                                    -Argument "$($username.PadRight(100))$($unsecurepassword.PadRight(64))"
+
+                                Write-Host "    Initialize OSA User"
+                                Invoke-NAVCodeunit `
+                                    -ServerInstance BC `
+                                    -CompanyName $companyName `
+                                    -CodeunitId 50189 `
+                                    -MethodName CreateOSAUser `
+                                    -Argument "$($username.PadRight(100))$($unsecurepassword.PadRight(64))"
+                            } else {
+                                Write-Host "    Skip app, app user and app license init as this seems to be a build container"
                             }
-
-                            Write-Host "    Initialize General User ($username / $unsecurepassword) in $companyName"
-                            Invoke-NAVCodeunit `
-                                -ServerInstance BC `
-                                -CompanyName $companyName `
-                                -CodeunitId 50189 `
-                                -MethodName CreateGeneralAppUser `
-                                -Argument "$($username.PadRight(100))$($unsecurepassword.PadRight(64))"
-
-                            Write-Host "    Initialize FSA User"
-                            Invoke-NAVCodeunit `
-                                -ServerInstance BC `
-                                -CompanyName $companyName `
-                                -CodeunitId 50189 `
-                                -MethodName CreateFSAUser `
-                                -Argument "$($username.PadRight(100))$($unsecurepassword.PadRight(64))"
-
-                            Write-Host "    Initialize OSA User"
-                            Invoke-NAVCodeunit `
-                                -ServerInstance BC `
-                                -CompanyName $companyName `
-                                -CodeunitId 50189 `
-                                -MethodName CreateOSAUser `
-                                -Argument "$($username.PadRight(100))$($unsecurepassword.PadRight(64))"
-                        } else {
-                            Write-Host "    Skip app, app user and app license init as this seems to be a build container"
                         }
                     }
                 }
