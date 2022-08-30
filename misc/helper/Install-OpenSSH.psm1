41ef118e72c8cdb747ea74873bb5a3d806fb095a
Patrick Grabensteiner <patrick.grabensteiner@cosmoconsult.com>
2022-08-30 11:04:44 +0200

    Update to latest Version of OpenSSH

--- misc/helper/Install-OpenSSH.psm1
+++ misc/helper/Install-OpenSSH.psm1
@@ -10,7 +10,7 @@ function Install-OpenSSH {
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
     Write-Output "Downloading OpenSSH"
-    Invoke-WebRequest "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.0.0.0p1-Beta/OpenSSH-Win64.zip" -OutFile OpenSSH-Win64.zip -UseBasicParsing
+    Invoke-WebRequest "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.9.1.0p1-Beta/OpenSSH-Win64.zip" -OutFile OpenSSH-Win64.zip -UseBasicParsing
 
     Write-Output "Expanding OpenSSH"
     Expand-Archive OpenSSH-Win64.zip C:\\
