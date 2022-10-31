e631e161e42f9921e8920dad7dd5e0c27d373be6
Patrick Schiefer <patrick.schiefer@cosmoconsult.com>
2022-10-31 14:43:30 +0100

    restructure

--- base/my/SetupConfiguration.ps1
+++ base/my/SetupConfiguration.ps1
@@ -1,13 +1,13 @@
 Write-Host "Start Setup Configuration"
 
-# invoke default
-. (Join-Path $runPath $MyInvocation.MyCommand.Name)
-
 $scripts = @(
                         (Join-Path $PSScriptRoot "EnablePerformanceCounter.ps1")
                    )
+Push-Location
+# invoke default
+. (Join-Path $runPath $MyInvocation.MyCommand.Name)
 
-
+Pop-Location
 
 foreach ($script in $scripts){
     if (Test-Path -Path $script) {
