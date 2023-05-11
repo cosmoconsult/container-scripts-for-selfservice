c2be703b5acd645bbb43d01a3a87df1feb4dcfc7
Tobias Fenster <tfenster@4psbau.de>
2023-05-11 17:16:49 +0200

    fix syntax

--- artifacts/4PS/Invoke-4PSArtifactHandling.ps1
+++ artifacts/4PS/Invoke-4PSArtifactHandling.ps1
@@ -73,9 +73,9 @@ function Invoke-4PSArtifactHandling {
                         Where-Object { 
                             if($env:IsBuildContainer -and !$_.Name.Contains('Test Automation')){
                                 "Skipping XML file {0} as it's no Test Automation database and it seems to be a build container" -f $_.FullName | Write-Host
-                                return false;
+                                return $false;
                             }
-                            return true;
+                            return $true;
                         } | Sort-Object Name -Descending
                     foreach ($demoDataFile in $files) {
                         $demoDataFileName = $demoDataFile | ForEach-Object { $_.Name }
