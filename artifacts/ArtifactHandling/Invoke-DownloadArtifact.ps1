ec071dc23bbe61a53a3fa20f6704352983741945
Tobias Fenster <tfenster@4psbau.de>
2023-07-18 08:54:54 +0200

    Clarify duplicate message

--- artifacts/ArtifactHandling/Invoke-DownloadArtifact.ps1
+++ artifacts/ArtifactHandling/Invoke-DownloadArtifact.ps1
@@ -134,7 +134,7 @@ function Invoke-DownloadArtifact {
                     Add-ArtifactsLog -message "Download Artifact from $($sourceUri)"
                 }
             } else {
-                Add-ArtifactsLog -message "Get Artifact from $sourceUri"
+                Add-ArtifactsLog -message "Copy Artifact from $sourceUri"
             }
 
             try {
@@ -147,7 +147,7 @@ function Invoke-DownloadArtifact {
                     }
                 } else {
                     if (Test-Path $sourceUri) {
-                        Add-ArtifactsLog -message "Get Artifact from $sourceUri"
+                        Add-ArtifactsLog -message "Found Artifact at $sourceUri"
                     } else {
                         Add-ArtifactsLog -message "No Artifact found at $sourceUri"
                     }                    
