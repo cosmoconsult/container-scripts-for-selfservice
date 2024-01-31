ad2a4f35c8fb1b4167ab367accf2052edfa5096b
Patrick Schiefer <patrick.schiefer@cosmoconsult.com>
2024-01-31 14:43:22 +0100

    fix

--- artifacts/AdditionalSetupArtifacts.ps1
+++ artifacts/AdditionalSetupArtifacts.ps1
@@ -198,8 +198,8 @@ $properties = @{}
 Invoke-LogEvent -name "AdditionalSetup - Started" -telemetryClient $telemetryClient
 
 Write-Host "Import BC.NuGetHelper"
-Install-Module -Name BC.NuGetHelper -Scope CurrentUser -Force
-Import-Module -Name BC.NuGetHelper -Scope Local
+Install-Module -Name "BC.NuGetHelper" -Scope CurrentUser -Force
+Import-Module -Name "BC.NuGetHelper" -Scope Local
 #TODO Import Trusted Feed from Config
 Write-Host "Setup trusted NugGet Feed"
 $bcContainerHelperConfig.TrustedNuGetFeeds += @([PSCustomObject]@{ "Url" = "https://pkgs.dev.azure.com/PGrabensteiner8351/Nugeticing_Test/_packaging/Nugeticing_Test/nuget/v3/index.json"; "Token" = "wz4zihliutaua6won5glzyzkt2qyf2qbvnwxhiykychvclrr2ldq"; "Patterns" = @('*'); "Fingerprints" = @() })
