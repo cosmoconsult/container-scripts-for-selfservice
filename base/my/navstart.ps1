665b46bc425e0e9df5131af4b0b157883aed584e
Patrick Schiefer <patrick.schiefer@cosmoconsult.com>
2024-05-14 19:06:25 +0200

    Switch completly to PWSH

--- base/my/navstart.ps1
+++ base/my/navstart.ps1
@@ -1,4 +1,5 @@
-Import-Module (Join-Path $PSScriptRoot "..\helper\k8s-bc-helper.psd1") -Scope Global
+#Import-Module (Join-Path $PSScriptRoot "..\helper\k8s-bc-helper.psd1") -Scope Global
+Invoke-Command { & "pwsh.exe"       } -NoNewScope # PowerShell 7
 $scripts = @(
                         (Join-Path $PSScriptRoot "ExtendedEnvironment.ps1"),
                         (Join-Path $PSScriptRoot "navstartCustomScripts.ps1"),
