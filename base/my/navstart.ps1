447aa996000827b39d01768f9aae5caa50ed3cb3
Patrick Schiefer <patrick.schiefer@cosmoconsult.com>
2024-05-14 22:24:16 +0200

    fix mypath

--- base/my/navstart.ps1
+++ base/my/navstart.ps1
@@ -1,8 +1,8 @@
 $version = [Version](Get-Item "C:\Program Files\Microsoft Dynamics NAV\*\Service\Microsoft.Dynamics.Nav.Server.exe").VersionInfo.FileVersion
 
 if ($version -ge [Version]"24.0.0.0") {
-    pwsh.exe (Join-Path $runPath "navstartCustom.ps1")
+    pwsh.exe (Join-Path $myPath "navstartCustom.ps1")
 } else {
-    . (Join-Path $runPath "navstartCustom.ps1")
+    . (Join-Path $myPath "navstartCustom.ps1")
 }
 
