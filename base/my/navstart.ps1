97a963727141ddf1d53e3d249d3f6c0629af2bcf
Patrick Schiefer <patrick.schiefer@cosmoconsult.com>
2024-05-14 19:15:14 +0200

    activate module

--- base/my/navstart.ps1
+++ base/my/navstart.ps1
@@ -1,4 +1,4 @@
-#Import-Module (Join-Path $PSScriptRoot "..\helper\k8s-bc-helper.psd1") -Scope Global
+Import-Module (Join-Path $PSScriptRoot "..\helper\k8s-bc-helper.psd1") -Scope Global
 Invoke-Command { & "pwsh.exe"       } -NoNewScope # PowerShell 7
 $scripts = @(
                         (Join-Path $PSScriptRoot "ExtendedEnvironment.ps1"),
