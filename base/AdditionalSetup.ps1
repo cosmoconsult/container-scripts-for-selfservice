99932dfa275230c644ad00ced45a100f513e3b93
Philipp Stöhr <philipp.stoehr@cosmoconsult.com>
2024-07-16 14:33:09 +0200

    move to additional setup

--- base/AdditionalSetup.ps1
+++ base/AdditionalSetup.ps1
@@ -5,6 +5,18 @@ $scripts = @(
 
 Write-Host "Start AdditionalSetup"
 
+if (!$TenantId) { $TenantId = "default" }
+$serverInstanceState = (Get-NAVServerInstance BC).State
+if ($serverInstanceState -ne "Running") {
+    Write-Host "Error: NAV ServerInstance not running, aborting..." -ForegroundColor Red
+    return
+}
+$TenantState = (Get-NavTenant -ServerInstance BC -Tenant $TenantId).State
+if ($TenantState -ne "Mounted" -and $TenantState -ne "Operational") {
+    Write-Host "Error: Tenant not mounted/operational, aborting..." -ForegroundColor Red
+    return
+}
+
 foreach ($script in $scripts) {
     if (Test-Path -Path $script) {
         Write-Host "Execute $script"
