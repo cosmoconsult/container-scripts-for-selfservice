40544c4cd5502db9034e72a23e498d71b84e0c31
Markus Lippert <lippertmarkus@gmx.de>
2024-08-16 11:16:48 +0200

    feat(additional-setup): add script to fix users with duplicate AuthenticationEmail (#127)

---
+++ misc/AdditionalSetupDuplicateUsers.ps1
@@ -0,0 +1,16 @@
+Write-Host "Finding users with duplicate AuthenticationEmail and fixing disabled duplicate users"
+
+$duplicateAadUserSets = Get-NAVServerUser -ServerInstance $ServerInstance | where { $_.AuthenticationEmail -ne '' } | group 'AuthenticationEmail' | Where { $_.Count -gt 1 } 
+
+foreach ($duplicateAadUserSet in $duplicateAadUserSets)
+{
+    $disabledDuplicateUsers = $duplicateAadUserSet.Group | Where-Object { $_.State -eq 'Disabled' }
+
+    foreach ($disabledDuplicateUser in $disabledDuplicateUsers)
+    {
+        Write-Host "Fixing AuthenticationEmail of duplicate user $($_.UserName)"
+
+        # moving to a non-existent email as removing doesn't work
+        Set-NAVServerUser -ServerInstance $ServerInstance -UserName $_.UserName -AuthenticationEmail "none@example.com"
+    }
+}
