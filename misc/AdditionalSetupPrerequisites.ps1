271b95cd174702696add3e200dd25b8351a14a18
Tobias Fenster <tfenster@4psbau.de>
2024-10-22 19:17:17 +0200

    disable choco and nodejs

--- misc/AdditionalSetupPrerequisites.ps1
+++ misc/AdditionalSetupPrerequisites.ps1
@@ -1,6 +1,7 @@
 Import-Module (Join-Path $PSScriptRoot "helper\k8s-bc-helper.psd1")
 Install-OpenSSH
 if ($env:IsBuildContainer) {
-    Install-Chocolatey
-    Install-Nodejs
+    # seems to kill output log handling in the pipeline
+    #Install-Chocolatey
+    #Install-Nodejs
 }
