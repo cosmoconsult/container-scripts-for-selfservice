6f40aa10bdc5585833ec998280025bb6cf4cfd6e
David Feldhoff <53570297+DavidFeldhoff@users.noreply.github.com>
2024-12-03 12:16:49 +0100

    Add `Set-AlpacaContainerKeyVaultAadAppAndCertificate` to be able to have keyvault access in containers (#155)
    Co-authored-by: David Feldhoff <dfeldhoff@4psbau.de>

--- base/my/SetupConfiguration.ps1
+++ base/my/SetupConfiguration.ps1
@@ -2,6 +2,7 @@ Write-Host "Start Setup Configuration"
 
 $scripts = @(
                         (Join-Path $runPath "EnablePerformanceCounter.ps1")
+                        (Join-Path $runPath "4PS/Set-AlpacaContainerKeyVaultAadAppAndCertificate.ps1")
 )
 Push-Location
 # invoke default
