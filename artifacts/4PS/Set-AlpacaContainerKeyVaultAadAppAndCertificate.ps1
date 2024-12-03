adb60917eb14083bf8c268399c4e5593c91bb2f7
David Feldhoff <dfeldhoff@4psbau.de>
2024-12-03 12:21:22 +0100

    fix ConvertTo-SecureString

--- artifacts/4PS/Set-AlpacaContainerKeyVaultAadAppAndCertificate.ps1
+++ artifacts/4PS/Set-AlpacaContainerKeyVaultAadAppAndCertificate.ps1
@@ -5,7 +5,7 @@ function Set-AlpacaContainerKeyVaultAadAppAndCertificate{
         $serverInstance = "BC",
         $enablePublisherValidation = $false,
         $pfxFile = "C:/azurefileshare/common/AlpacaContainerKeyVaultReader.pfx",
-        $pfxPassword = ConvertTo-SecureString -String "P@ssw0rd" -AsPlainText -Force,
+        $pfxPassword = (ConvertTo-SecureString -String "P@ssw0rd" -AsPlainText -Force),
         $clientId = "6efd7c53-94d3-489e-95a4-30e25de612ba",
         $doNotRestartServiceTier = $false
     )
