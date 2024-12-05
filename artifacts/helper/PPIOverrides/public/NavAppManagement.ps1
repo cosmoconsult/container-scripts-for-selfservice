343dc2f9cece2eff02b2696721dc246475e5c396
Ole Wunschmann <owunschmann4046@cosmoconsult.com>
2024-12-05 13:00:40 +0100

    Override more NAV App Management Functions

--- artifacts/helper/PPIOverrides/public/NavAppManagement.ps1
+++ artifacts/helper/PPIOverrides/public/NavAppManagement.ps1
@@ -43,3 +43,102 @@ function Publish-NAVApp() {
     }
 }
 Export-ModuleMember -Function Publish-NAVApp
+
+function Sync-NAVApp() {
+    [CmdletBinding()]
+    Param()
+
+    DynamicParam {
+        $overwrittenParameters = Invoke-CommandInPwshCore -ScriptBlock {
+            if (! (Get-Module 'Microsoft.BusinessCentral.Apps.Management')) {
+                c:\run\prompt.ps1 -silent
+            }
+            (Get-Command Sync-NAVApp).Parameters
+        }
+        ConvertTo-DynamicParameters -CommandName 'Microsoft.BusinessCentral.Apps.Management\Sync-NAVApp' -Parameters $overwrittenParameters
+    }
+
+    begin {
+        $MyInvocation.MyCommand.Parameters.Values | Where-Object { ! $_.IsDynamic } | Foreach-Object {
+            $PSBoundParameters.Remove($_.Name) | Out-Null
+        }
+    }
+    
+    process {
+        $pwshCoreSession = Request-PwshCoreSession
+        if (!$pwshCoreSession) { return }
+        Invoke-Command -Session $pwshCoreSession -ScriptBlock {
+            if (! (Get-Module 'Microsoft.BusinessCentral.Apps.Management')) {
+                c:\run\prompt.ps1 -silent
+            }
+            Sync-NAVApp @using:PSBoundParameters
+        }
+    }
+}
+Export-ModuleMember -Function Sync-NAVApp
+
+function Install-NAVApp() {
+    [CmdletBinding()]
+    Param()
+
+    DynamicParam {
+        $overwrittenParameters = Invoke-CommandInPwshCore -ScriptBlock {
+            if (! (Get-Module 'Microsoft.BusinessCentral.Apps.Management')) {
+                c:\run\prompt.ps1 -silent
+            }
+            (Get-Command Install-NAVApp).Parameters
+        }
+        ConvertTo-DynamicParameters -CommandName 'Microsoft.BusinessCentral.Apps.Management\Install-NAVApp' -Parameters $overwrittenParameters
+    }
+
+    begin {
+        $MyInvocation.MyCommand.Parameters.Values | Where-Object { ! $_.IsDynamic } | Foreach-Object {
+            $PSBoundParameters.Remove($_.Name) | Out-Null
+        }
+    }
+    
+    process {
+        $pwshCoreSession = Request-PwshCoreSession
+        if (!$pwshCoreSession) { return }
+        Invoke-Command -Session $pwshCoreSession -ScriptBlock {
+            if (! (Get-Module 'Microsoft.BusinessCentral.Apps.Management')) {
+                c:\run\prompt.ps1 -silent
+            }
+            Install-NAVApp @using:PSBoundParameters
+        }
+    }
+}
+Export-ModuleMember -Function Install-NAVApp
+
+function Start-NAVAppDataUpgrade() {
+    [CmdletBinding()]
+    Param()
+
+    DynamicParam {
+        $overwrittenParameters = Invoke-CommandInPwshCore -ScriptBlock {
+            if (! (Get-Module 'Microsoft.BusinessCentral.Apps.Management')) {
+                c:\run\prompt.ps1 -silent
+            }
+            (Get-Command Start-NAVAppDataUpgrade).Parameters
+        }
+        ConvertTo-DynamicParameters -CommandName 'Microsoft.BusinessCentral.Apps.Management\Start-NAVAppDataUpgrade' -Parameters $overwrittenParameters
+    }
+
+    begin {
+        $MyInvocation.MyCommand.Parameters.Values | Where-Object { ! $_.IsDynamic } | Foreach-Object {
+            $PSBoundParameters.Remove($_.Name) | Out-Null
+        }
+    }
+    
+    process {
+        $pwshCoreSession = Request-PwshCoreSession
+        if (!$pwshCoreSession) { return }
+        Invoke-Command -Session $pwshCoreSession -ScriptBlock {
+            if (! (Get-Module 'Microsoft.BusinessCentral.Apps.Management')) {
+                c:\run\prompt.ps1 -silent
+            }
+            Start-NAVAppDataUpgrade @using:PSBoundParameters
+        }
+    }
+}
+Export-ModuleMember -Function Start-NAVAppDataUpgrade
