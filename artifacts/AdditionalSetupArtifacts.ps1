b5180036fc1a3b1abed56b8b117ce3d5975a3538
Ole Wunschmann <owunschmann4046@cosmoconsult.com>
2024-12-11 23:24:03 +0100

    Override all NAV App Management Functions

--- artifacts/AdditionalSetupArtifacts.ps1
+++ artifacts/AdditionalSetupArtifacts.ps1
@@ -168,10 +168,12 @@ Invoke-LogEvent -name "AdditionalSetup - Started" -telemetryClient $telemetryCli
 
 # Show installed apps
 Write-Host "##[group]Initially installed apps"
-Get-NAVAppInfo -Tenant $tenantId -TenantSpecificProperties -ServerInstance $ServerInstance | 
-    Select-Object Name, Publisher, Version, Scope, IsPublished, IsInstalled, SyncState, NeedsUpgrade, ExtensionDataVersion | 
-    Format-Table -AutoSize | 
-    Out-String -Width 1024
+Measure-Command {
+    Get-NAVAppInfo -Tenant $tenantId -TenantSpecificProperties -ServerInstance $ServerInstance | 
+        Select-Object Name, Publisher, Version, Scope, IsPublished, IsInstalled, SyncState, NeedsUpgrade, ExtensionDataVersion | 
+        Format-Table -AutoSize | 
+        Out-String -Width 1024
+} | Format-Table
 Write-Host "##[endgroup]"
 
 # Download Artifacts
