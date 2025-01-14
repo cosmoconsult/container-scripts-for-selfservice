2a87b91e6328905042631430826244d7a4817233
David Feldhoff <dfeldhoff@4psbau.de>
2025-01-14 17:04:01 +0100

    Temporarily use W. Europe Standard Time as ServicesDefaultTimeZone

--- base/my/SetupConfiguration.ps1
+++ base/my/SetupConfiguration.ps1
@@ -4,6 +4,9 @@ $scripts = @(
                         (Join-Path $runPath "EnablePerformanceCounter.ps1")
                         (Join-Path $runPath "4PS/Set-AlpacaContainerKeyVaultAadAppAndCertificate.ps1")
 )
+Write-Host "Set-NAVServerConfiguration -KeyName ServicesDefaultTimeZone -KeyValue `"W. Europe Standard Time`" -ServerInstance BC"
+Set-NAVServerConfiguration -KeyName ServicesDefaultTimeZone -KeyValue "W. Europe Standard Time" -ServerInstance BC
+
 Push-Location
 # invoke default
 . (Join-Path $runPath $MyInvocation.MyCommand.Name)
