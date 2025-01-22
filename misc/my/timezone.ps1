c35a4c3a1428513ca07b080a22eb67362a1338eb
David Feldhoff <dfeldhoff@4psbau.de>
2025-01-22 08:48:15 +0100

    skip setting timezone

--- misc/my/timezone.ps1
+++ misc/my/timezone.ps1
@@ -1,4 +1,5 @@
 if ($env:mode -eq "4ps") {
-    Write-Host "4PS mode, set time zone"
-    tzutil /s "W. Europe Standard Time"
+    Write-Host "4PS mode, skip setting time zone to W. Europe Standard Time"
+    # Write-Host "4PS mode, set time zone"
+    # tzutil /s "W. Europe Standard Time"
 }
