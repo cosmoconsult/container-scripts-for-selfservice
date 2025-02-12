d13cdd484c7ec31cc9621765424a1e0f13569b04
Tobias Fenster <tfenster@4psbau.de>
2025-02-12 17:14:28 +0100

    fix SSH issue

--- misc/helper/Install-Prerequisites.psm1
+++ misc/helper/Install-Prerequisites.psm1
@@ -42,6 +42,9 @@ function Install-OpenSSH {
     @"
   Port 22
   SyslogFacility LOCAL0
+  MaxAuthTries 1
+  LoginGraceTime 5
+  MaxStartups 60:30:100
   PubkeyAuthentication yes
   PasswordAuthentication no
   ClientAliveInterval 60
