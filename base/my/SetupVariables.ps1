b6159b521742b11e8c7f15da12f1adb72bef487f
Philipp Stöhr <philipp.stoehr@cosmoconsult.com>
2025-02-25 14:33:46 +0100

    revert testing

--- base/my/SetupVariables.ps1
+++ base/my/SetupVariables.ps1
@@ -1,6 +1,6 @@
 if (($env:cosmoServiceRestart -eq $false) -and ![string]::IsNullOrWhiteSpace($env:bakfile)) {
     Write-Host "Increasing SQL timeout to 600 seconds for database backup restore"
-    $env:SqlTimeout = "2"
+    $env:SqlTimeout = "600"
 }
 
 # invoke default
