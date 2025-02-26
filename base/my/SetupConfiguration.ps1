22eafaf08ab720ed0d4c728cb08cb208ab5822eb
OleWunschmann <44057549+OleWunschmann@users.noreply.github.com>
2025-02-26 11:46:01 +0100

    fix(nextmajor): reenable configuration "ServerFileCacheDirectory" for BC26 (#172)
    Co-authored-by: Ole Wunschmann <owunschmann4046@cosmoconsult.com>

--- base/my/SetupConfiguration.ps1
+++ base/my/SetupConfiguration.ps1
@@ -7,15 +7,6 @@ Push-Location
 # invoke default
 . (Join-Path $runPath $MyInvocation.MyCommand.Name)
 
-# Workaround for BC26 (NextMajor)
-$version = [Version](Get-Item "C:\Program Files\Microsoft Dynamics NAV\*\Service\Microsoft.Dynamics.Nav.Server.exe").VersionInfo.FileVersion
-if ($version -and $version.Major -eq 26) {
-    if (Get-NAVServerConfiguration -ServerInstance $ServerInstance -KeyName 'ServerFileCacheDirectory') { 
-        Write-Host "Resetting ServerFileCacheDirectory for BC26"
-        Set-NAVServerConfiguration -ServerInstance $ServerInstance -KeyName 'ServerFileCacheDirectory' -KeyValue '' -WA SilentlyContinue
-    }
-}
-
 Pop-Location
 
 
