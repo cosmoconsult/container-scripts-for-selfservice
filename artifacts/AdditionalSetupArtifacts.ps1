cb590ce1bbdcdc0a1cfc887235795e988cca8738
Tobias Fenster <tfenster@4psbau.de>
2025-02-28 14:09:01 +0100

    try to continue on error

--- artifacts/AdditionalSetupArtifacts.ps1
+++ artifacts/AdditionalSetupArtifacts.ps1
@@ -518,8 +518,8 @@ if (($env:cosmoServiceRestart -eq $false) -and ![string]::IsNullOrEmpty($env:saa
         -Force
 
     Write-Host " - Upgrading tenant"
-    Start-NAVDataUpgrade -SkipUserSessionCheck -FunctionExecutionMode Serial -ServerInstance BC -SkipAppVersionCheck -Force -ErrorAction Stop -Tenant $TenantId
-    Wait-DataUpgradeToFinish -ServerInstance BC -ErrorAction Stop -Tenant $TenantId -Retry
+    Start-NAVDataUpgrade -SkipUserSessionCheck -FunctionExecutionMode Serial -ContinueOnError -ServerInstance BC -SkipAppVersionCheck -Force -ErrorAction Stop -Tenant $TenantId
+    Wait-DataUpgradeToFinish -ServerInstance BC -ErrorAction Stop -Tenant $TenantId
 
     Write-Host " - Check data upgrade is executed"
     Set-NavServerInstance -ServerInstance BC -Restart
