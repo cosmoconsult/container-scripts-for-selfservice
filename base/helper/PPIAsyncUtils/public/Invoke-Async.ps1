0c2b02de20798fede6da9ed1f89f45ba317abd0e
Ole Wunschmenn <owunschmann4046@cosmoconsult.com>
2025-03-12 22:01:42 +0100

    Download Artifacts (async)

--- base/helper/PPIAsyncUtils/public/Invoke-Async.ps1
+++ base/helper/PPIAsyncUtils/public/Invoke-Async.ps1
@@ -6,6 +6,7 @@ function Invoke-Async {
         [hashtable]$Parameters = @{},
         [System.Management.Automation.Runspaces.RunspacePool]$RunspacePool
     )
+    Write-Host "Invoke-Async - Begin"
     $powershell = [powershell]::Create();
     $powershell.RunspacePool = $RunspacePool;
 
@@ -24,6 +25,7 @@ function Invoke-Async {
     $powershell.AddParameter("ScriptBlock", $ScriptBlock) | Out-Null;
     $powershell.AddParameter("Parameters", $Parameters.Clone()) | Out-Null;
 
+    Write-Host "Invoke-Async - BeginInvoke"
     $result = $powershell.BeginInvoke();
     
     $runspace = [pscustomobject]@{
@@ -34,5 +36,7 @@ function Invoke-Async {
     $runspace
 
     $script:runspaces += $runspace
+
+    Write-Host "Invoke-Async - End"
 }
 Export-ModuleMember -Function Invoke-Async
