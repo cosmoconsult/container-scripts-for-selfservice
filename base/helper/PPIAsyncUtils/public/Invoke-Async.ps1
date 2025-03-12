c2b5b5e6b983475749a4f72413225dcc8294d9e8
Ole Wunschmenn <owunschmann4046@cosmoconsult.com>
2025-03-12 22:00:39 +0100

    Download Artifacts (async)

--- base/helper/PPIAsyncUtils/public/Invoke-Async.ps1
+++ base/helper/PPIAsyncUtils/public/Invoke-Async.ps1
@@ -16,12 +16,12 @@ function Invoke-Async {
             [hashtable]$Parameters = @{}
         );
         try {
-            . $ScriptBlock @Parameters *>&1
+            . ( [scriptblock]::Create($ScriptBlock) ) @Parameters *>&1
         } catch {
             $_
         }
     }) | Out-Null;
-    $powershell.AddParameter("ScriptBlock", $ScriptBlock.GetNewClosure()) | Out-Null;
+    $powershell.AddParameter("ScriptBlock", $ScriptBlock) | Out-Null;
     $powershell.AddParameter("Parameters", $Parameters.Clone()) | Out-Null;
 
     $result = $powershell.BeginInvoke();
