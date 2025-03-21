9deec48403a4c7a7bd4e51b1807b02c4fcc827e7
Ole Wunschmenn <owunschmann4046@cosmoconsult.com>
2025-03-22 00:24:38 +0100

    Download Artifacts (async)

--- artifacts/ArtifactHandling/NuGet/Import-NuGetTools.ps1
+++ artifacts/ArtifactHandling/NuGet/Import-NuGetTools.ps1
@@ -22,8 +22,8 @@
 
         Write-Host "Set trusted NuGet feeds of BCContainerHelperConfig"
         $bcContainerHelperConfig.TrustedNuGetFeeds = @(
-            Compare-Object -ReferenceObject $Feeds -DifferenceObject $bcContainerHelperConfig.TrustedNuGetFeeds -Property Url -IncludeEqual -PassThru | 
-                Select-Object -Property * -ExcludeProperty SideIndicator
+            Compare-Object -ReferenceObject $TrustedFeeds -DifferenceObject $bcContainerHelperConfig.TrustedNuGetFeeds -IncludeEqual | 
+                Select-Object -ExpandProperty InputObject
         )
     }
 }
