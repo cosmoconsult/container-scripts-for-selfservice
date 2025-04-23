6181426da3cb8864c0960ba01c20a3fd21d2e073
David Feldhoff <dfeldhoff@4psbau.de>
2025-04-23 13:57:57 +0200

    use `4PS Construct ??` instead of `4PS Construct DE` when trying to find out if mod. base app is going to be installed

--- artifacts/AdditionalSetupArtifacts.ps1
+++ artifacts/AdditionalSetupArtifacts.ps1
@@ -176,7 +176,7 @@ if ($global:cosmoRunspacePool) {
     }
 }
 
-$installModifiedBaseAppManually = $null -ne ( $global:cosmoArtifacts.Artifacts.All | Where-Object { $null -ne $_.name -and $_.name -like "*_4PS Construct DE_*" } )
+$installModifiedBaseAppManually = $null -ne ( $global:cosmoArtifacts.Artifacts.All | Where-Object { $null -ne $_.name -and $_.name -like "*_4PS Construct ??_*" } )
 $installModifiedBaseAppManually = $installModifiedBaseAppManually -or ![string]::IsNullOrEmpty($env:systemAppOnly)
 
 # Initialize company
