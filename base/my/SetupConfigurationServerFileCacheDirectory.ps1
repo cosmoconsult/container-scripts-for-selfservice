55fe2baf82b3c6e7d4b069d2883cf7f26659a3d5
Ole Wunschmenn <owunschmann4046@cosmoconsult.com>
2025-07-01 13:16:41 +0200

    feat(persistence): Copy original server file cache to persistent directory

--- base/my/SetupConfigurationServerFileCacheDirectory.ps1
+++ base/my/SetupConfigurationServerFileCacheDirectory.ps1
@@ -4,20 +4,22 @@ $CustomConfig = [xml](Get-Content $CustomConfigFile)
 $serverFileCacheDirectory = $customConfig.SelectSingleNode("//appSettings/add[@key='ServerFileCacheDirectory']").Value
 
 if (! $serverFileCacheDirectory) {
+    Write-Host "ServerFileCacheDirectory not defined in Service Tier Config"
     return;
 }
 
 if (Test-Path -Path $serverFileCacheDirectory) {
-    Write-Host "ServerFileCacheDirectory already exists: $serverFileCacheDirectory"
+    Write-Host "ServerFileCacheDirectory already exists: '$serverFileCacheDirectory'"
     return;
 }
  
 $originalServerFileCacheDirectory = (Get-Item "C:\ProgramData\Microsoft\Microsoft Dynamics NAV\*\Server\${NavServiceName}" -ea SilentlyContinue).FullName
 if (! $originalServerFileCacheDirectory) {
+    Write-Host "Original Server File Cache Directory not found. Expected: 'C:\ProgramData\Microsoft\Microsoft Dynamics NAV\*\Server\${NavServiceName}'"
     return;
 }
 
-Write-Host "Copy Server File Cache from ${originalServerFileCacheDirectory} to ${serverFileCacheDirectory}"
+Write-Host "Copy Server File Cache from '${originalServerFileCacheDirectory}' to '${serverFileCacheDirectory}'"
 $duration = Measure-Command { 
     Copy-Item -Path $originalServerFileCacheDirectory -Destination $serverFileCacheDirectory -Recurse 
 }
