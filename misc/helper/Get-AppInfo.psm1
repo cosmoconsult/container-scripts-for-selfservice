6b517eaee70abf1ff1c820830affdad9fdbc3949
Christoph Blank <christoph.blank@cosmoconsult.com>
2025-10-14 08:36:18 +0200

    feat: Add Get-AppInfo function to retrieve app information as JSON AB#4489

---
+++ misc/helper/Get-AppInfo.psm1
@@ -0,0 +1,28 @@
+<#
+ .Synopsis
+  Get the App information as json
+ .Description
+  Get the App information as json file and saves it to C:\inetpub\wwwroot\http
+ .Example
+  Get-AppInfo
+#>
+function Get-AppInfo {
+    [CmdletBinding()]
+    Param ()
+
+    Write-Host "Getting app info"
+
+    $appInfoFolder = "C:\inetpub\wwwroot\http"
+    if (!(Test-Path $appInfoFolder)) {
+        New-Item $appInfoFolder -ItemType Directory | Out-Null
+    }
+    $appInfoFilename = ("appInfo" + [DateTime]::Now.ToString("yyyy-MM-ddHH.mm.ss") + ".json")
+    $appInfoPath = Join-Path $appInfoFolder $appInfoFilename
+  
+    Get-NavAppInfo -ServerInstance BC -Tenant default -TenantSpecificProperties | Select-Object @{Name = 'AppId'; Expression = { $_.AppId.ToString() } }, Name, Publisher, @{Name = 'Version'; Expression = { $_.Version.ToString() } } | ConvertTo-Json -Compress | Out-File -FilePath $appInfoPath -Encoding utf8
+    
+    Write-Host "got appinfo"
+    Write-Host ("Apps:" + $appInfoFilename)
+}
+
+Export-ModuleMember -Function Get-AppInfo
