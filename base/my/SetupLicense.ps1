9e62f30bbe6ef15f6d3e62ddb9d917d2ac255c01
Tobias Fenster <tfenster@4psbau.de>
2025-10-22 14:48:33 +0200

    fix shutdown

--- base/my/SetupLicense.ps1
+++ base/my/SetupLicense.ps1
@@ -3,7 +3,7 @@ function HandleIssue {
     if ($null -ne $env:ArtifactUrl -and $env:ArtifactUrl.EndsWith('onprem/26.2.34746.34832/w1')) {
         Invoke-LogEvent -name "SetupLicense, BC Service is not running on a OnPrem 26.2 W1 container" -telemetryClient $telemetryClient
         Write-Error "BC Service is not running on a OnPrem 26.2 W1 container. This is a known issue, typically fixed by a restart. Doing that now."
-        exit 1
+        Stop-Computer -Force
     }
     else {
         Invoke-LogEvent -name "SetupLicense, BC Service is not running, but this is NOT a OnPrem 26.2 W1 container" -telemetryClient $telemetryClient
