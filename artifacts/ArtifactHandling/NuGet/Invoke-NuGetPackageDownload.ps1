3d5e5128e92dd4ea8ddfbb74cb754fc838b8f11c
Philipp Stöhr <52424849+ps610@users.noreply.github.com>
2025-12-08 13:25:25 +0100

    fix(NuGet): add package version mormalization for single-part versions (#215)
    fixes [AB#4579](https://dev.azure.com/cc-ppi/83f75d99-795d-45dc-8543-9fe1918ff7f9/_workitems/edit/4579)

--- artifacts/ArtifactHandling/NuGet/Invoke-NuGetPackageDownload.ps1
+++ artifacts/ArtifactHandling/NuGet/Invoke-NuGetPackageDownload.ps1
@@ -55,9 +55,17 @@ function Invoke-NuGetPackageDownload() {
             
             if ($Version) {
                 if ($Version -match $versionPattern) {
+                    # Normalize version parts to ensure at least major.minor format
+                    $versionParts = $matches.version.Split('.')
+                    if ($versionParts.Count -eq 1) {
+                        $normalizedVersion = "{0}.0" -f $versionParts[0]
+                    } else {
+                        $normalizedVersion = $matches.version
+                    }
+
                     # Convert NuGet version to a range (from version, to excl. version + 1)
-                    $fromVersion  = '{0}{1}' -f $matches.version, $matches.prerelease
-                    $toVersion    = '{0}{1}' -f ( $matches.version -replace '\d+$', ( [int]$matches.version.Split('.')[-1] + 1 ) ), $matches.prerelease
+                    $fromVersion  = '{0}{1}' -f $normalizedVersion, $matches.prerelease
+                    $toVersion    = '{0}{1}' -f ( $normalizedVersion -replace '\d+$', ( [int]$normalizedVersion.Split('.')[-1] + 1 ) ), $matches.prerelease
                     $versionRange = '[{0},{1})' -f $fromVersion, $toVersion
                     Write-Host "Converted version '$Version' to NuGet version range '$versionRange'"
                 } else {
