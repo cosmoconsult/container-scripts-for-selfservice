e895c9337c488cefc429e3c1c921c0b4de7c6a3a
Christoph Blank <christoph.blank@cosmoconsult.com>
2026-03-24 15:28:59 +0100

    fix(Get-ArtifactsFromEnvironment): redact PAT in log output for security

--- artifacts/ArtifactHandling/Get-ArtifactsFromEnvironment.ps1
+++ artifacts/ArtifactHandling/Get-ArtifactsFromEnvironment.ps1
@@ -62,7 +62,7 @@ function Get-ArtifactsFromEnvironment {
                     $artifactJson = '{"artifacts":[]}'
                 }
 
-                Write-Host "Artifacts: $artifactJson"
+                Write-Host "Artifacts: $($artifactJson -replace '("pat": ")(.*)(")','$1***REDACTED***$3')" # hide pat in logs
                 $envArtifacts = ($artifactJson | ConvertFrom-Json -ErrorAction SilentlyContinue)
                 $artifacts = $envArtifacts.artifacts
                 if (! $artifacts) {
