9a14e157cc574a037d3ee6d96c6279f470351f4d
Philipp Stöhr <philipp.stoehr@cosmoconsult.com>
2026-05-12 10:33:14 +0200

    ignore plain names

--- misc/AdditionalSetupAdditionalOwners.ps1
+++ misc/AdditionalSetupAdditionalOwners.ps1
@@ -28,6 +28,10 @@ Wait-NAVTenantReady -ServerInstance $ServerInstance -Tenant $tenantId -Retries 6
 
 
 foreach ($account in $accounts) {
+    if (-not $navuserpasswordAuth -and $account -notlike "*@*") {
+        Write-Host "  Skipping account '$account': does not contain '@', not valid for AAD auth."
+        continue
+    }
     Write-Host "  Processing account: $account"
     $BcUser = $null
     $shortenedAccount = $account.Split("@")[0]
