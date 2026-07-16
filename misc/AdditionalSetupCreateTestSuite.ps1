79404035b8efc86d1e715e9eb994dfd87230e598
OleWunschmann <OleWunschmann@users.noreply.github.com>
2026-07-16 18:30:21 +0200

    feat: extended logging for test suite create

--- misc/AdditionalSetupCreateTestSuite.ps1
+++ misc/AdditionalSetupCreateTestSuite.ps1
@@ -1,37 +1,54 @@
-Write-Host "Create DEFAULT Test Suite"
+Write-Host "::group::Create DEFAULT Test Suite"
 
-Write-Host "Collecting information about the current user, server instance, tenant, and company..."
+Write-Host "Collecting information about the server instance, tenant and companies..."
+
+Write-Host "Server instance: $ServerInstance"
+if ($tenantParam.ContainsKey('Tenant')) {
+    Write-Host "Tenant: $($tenantParam.Tenant)"
+} else {
+    Write-Host "Tenant: None (single-tenant scope)"
+}
+$companies = @(Get-NAVCompany -ServerInstance $ServerInstance @tenantParam | Select-Object -ExpandProperty CompanyName | Where-Object { $_ -ne "My Company" })
+Write-Host "Companies: $($companies -join ', ')"
+
+Write-Host "Ensure OS user is a BC user and has the SUPER permission set..."
+
 $me = whoami
-$Companies = Get-NAVCompany -ServerInstance $ServerInstance @tenantParam | Where-Object { $_.CompanyName -ne "My Company" }
-$myaccount = Get-NAVServerUser -ServerInstance $ServerInstance @tenantParam | Where-Object { $_.UserName -eq $me }
+Write-Host "OS user: $me"
+$bcUser = Get-NAVServerUser -ServerInstance $ServerInstance @tenantParam | Select-Object -ExpandProperty UserName | Where-Object { $_ -eq $me } | Select-Object -First 1
+Write-Host "BC user: $bcUser"
 
-if ($null -eq $myaccount) {
-    Write-Host "Adding $me as a user to the tenant $tenantId and assigning SUPER permission set"
+if ($null -eq $bcUser) {
+    Write-Host "Adding BC user $me"
     New-NAVServerUser -ServerInstance $ServerInstance @tenantParam -WindowsAccount $me
+}
+
+$bcUserPermissionSets = @(Get-NAVServerUserPermissionSet -ServerInstance $ServerInstance @tenantParam -WindowsAccount $me | Select-Object -ExpandProperty PermissionSetID)
+Write-Host "BC user permission sets: $($bcUserPermissionSets -join ', ')"
+
+if ($bcUserPermissionSets -notcontains "SUPER") {
+    Write-Host "Granting BC user the SUPER permission set"
     New-NAVServerUserPermissionSet -WindowsAccount $me -PermissionSetId SUPER -ServerInstance $ServerInstance @tenantParam
 }
 
-foreach ($Company in $Companies) {
-    Write-Host "Creating DEFAULT Test Suite in the company $($Company.CompanyName) of the tenant $tenantId"
+foreach ($company in $companies) {
     try {
-        Invoke-NAVCodeunit -ServerInstance $ServerInstance @tenantParam -CompanyName $Company.CompanyName -CodeunitId 130456 -MethodName 'CreateTestSuite' -Argument 'DEFAULT' -ErrorAction Stop
+        Write-Host "Creating DEFAULT Test Suite in the company $company"
+        Invoke-NAVCodeunit -ServerInstance $ServerInstance @tenantParam -CompanyName $company -CodeunitId 130456 -MethodName 'CreateTestSuite' -Argument 'DEFAULT' -ErrorAction Stop
     }
     catch {
         Write-Host "Error creating DEFAULT Test Suite: $($_.Exception.Message)"
     }
 }
 
-if ($null -eq $myaccount) {
-    if ($tenantParam.ContainsKey('Tenant')) {
-        Write-Host "Removing user $me in tenant $($tenantParam.Tenant)"
-    }
-    else {
-        Write-Host "Removing user $me (single-tenant scope)"
-    }
+if ($null -eq $bcUser) {
     try {
+        Write-Host "Removing BC user $me"
         Remove-NAVServerUser -ServerInstance $ServerInstance @tenantParam -WindowsAccount $me
     }
     catch {
         Write-Host "Error removing user: $($_.Exception.Message)"
     }
 }
+
+Write-Host "::endgroup::"
