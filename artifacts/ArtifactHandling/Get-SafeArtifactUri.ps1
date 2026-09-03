e90349f956fda849ae2fb86b7d2f3a02ab8a3661
Philipp Stöhr <philipp.stoehr@cosmoconsult.com>
2026-09-03 15:14:36 +0200

    fix

--- artifacts/ArtifactHandling/Get-SafeArtifactUri.ps1
+++ artifacts/ArtifactHandling/Get-SafeArtifactUri.ps1
@@ -3,9 +3,9 @@ function Get-SafeArtifactUri {
         [string] $Uri
     )
 
-    $safeUri = "$Uri" -replace '([?&]pat=)[^&]*', '$1***REDACTED***' # Redact PAT query parameters
+    $safeUri = "$Uri" -replace '([?&]pat=)[^&#"]*', '$1***REDACTED***' # Redact PAT query parameters
     $safeUri = $safeUri -replace '("pat"\s*:\s*")[^"]*(")', '$1***REDACTED***$2' # Redact PAT values in artifact JSON
-    $safeUri = $safeUri -replace '(/filebrowser/api/public/dl/)[^/?#]+', '$1***REDACTED***' # Redact filebrowser download tokens
+    $safeUri = $safeUri -replace '(/filebrowser/api/public/dl/)[^/?#"]+', '$1***REDACTED***' # Redact filebrowser download tokens
     return $safeUri
 }
 
