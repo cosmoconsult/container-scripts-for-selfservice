98069c4466fce4d93a87db5d7e4eebd1bc083ab0
Philipp Stöhr <philipp.stoehr@cosmoconsult.com>
2026-09-07 08:32:21 +0200

    redact SAS signature query parameters

--- artifacts/ArtifactHandling/Get-SafeArtifactUri.ps1
+++ artifacts/ArtifactHandling/Get-SafeArtifactUri.ps1
@@ -6,6 +6,7 @@ function Get-SafeArtifactUri {
     $safeUri = "$Uri" -replace '([?&]pat=)[^&#"]*', '$1***REDACTED***' # Redact PAT query parameters
     $safeUri = $safeUri -replace '("pat"\s*:\s*")[^"]*(")', '$1***REDACTED***$2' # Redact PAT values in artifact JSON
     $safeUri = $safeUri -replace '(/filebrowser/api/public/dl/)[^/?#"]+', '$1***REDACTED***' # Redact filebrowser download tokens
+    $safeUri = $safeUri -replace '([?&]sig=)[^&#"]*', '$1***REDACTED***' # Redact SAS signature query parameters
     return $safeUri
 }
 
